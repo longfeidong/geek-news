bffc89b2defb0850329fc240ee63dec7e9063a2c
donglongfei <donglongfei2011@gmail.com>
2020-11-19 14:16:22 +0800

    h5 layout finish

---
+++ components/navbar/navbar.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="navbar">
+		<view class="navbar-fixed">
+			<view class="navbar-search">
+				<view class="navbar-search_icon"></view>
+				<view class="navbar-search_text">uniapp、react</view>
+			</view>
+		</view>
+		<view style="height: 45px;"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.navbar {
+		.navbar-fixed {
+			position: fixed;
+			left: 0;
+			top: 0;
+			z-index: 999;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+			height: 45px;
+			padding: 0 15px;
+			box-sizing: border-box;
+			background-color: $gk-base-color;
+			.navbar-search {
+				display: flex;
+				align-items: center;
+				width: 100%;
+				height: 30px;
+				padding: 0 10px;
+				background-color: #FFFFFF;
+				border-radius: 30px;
+				.navbar-search_icon {
+					width: 10px;
+					height: 10px;
+					border: 1px solid red;
+					margin-right: 10px;
+				}
+				.navbar-search_text {
+					font-size: 14px;
+					color: #666;
+				}
+			}
+		}
+	}
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,11 +1,18 @@
 <template>
 	<view>
-		
+		<!-- 自定义导航栏 -->
+		<navbar></navbar>
+		<view v-for="item in 100">{{item}}内容数据</view>
 	</view>
 </template>
 
 <script>
+	// easycom @/components/组件名/组件名.vue 局部引入
+	// import navbar from '@/components/navbar/navbar.vue'
 	export default {
+		// components: {
+		// 	navbar
+		// },
 		data() {
 			return {
 				
