669485eff4fa3dc6b6d17173fd528e730ba181d4
donglongfei <donglongfei2011@gmail.com>
2020-11-20 15:59:01 +0800

    微信小程序navbar优化

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -1,9 +1,14 @@
 <template>
 	<view class="navbar">
 		<view class="navbar-fixed">
-			<view class="navbar-search">
-				<view class="navbar-search_icon"></view>
-				<view class="navbar-search_text">uniapp、react</view>
+			<!-- 状态栏 -->
+			<view class="status-bar" :style="{height: statusBarHeight + 'px'}"></view>
+			<!-- 搜索框 -->
+			<view class="navbar-content" :style="{height: navBarHeight + 'px', width: navBarWidth + 'px'}">
+				<view class="navbar-search">
+					<view class="navbar-search_icon"></view>
+					<view class="navbar-search_text">uniapp、react</view>
+				</view>
 			</view>
 		</view>
 		<view style="height: 45px;"></view>
@@ -14,8 +19,30 @@
 	export default {
 		data() {
 			return {
-				
+				statusBarHeight: 20,
+				navBarHeight: 45,
+				navBarWidth: 375
 			};
+		},
+		created() {
+			// 获取手机系统信息
+			const mInfo = uni.getSystemInfoSync()
+			this.statusBarHeight = mInfo.statusBarHeight
+			console.log(mInfo.statusBarHeight)
+			// H5 APP Ali-pay navBar宽度
+			this.navBarWidth = mInfo.windowWidth
+			
+			// H5 APP Ali-pay不支持这个API
+			// #ifndef H5 || APP-PLUS || MP-ALIPAY
+			// 获取微信小程序胶囊的位置信息
+			const miniMenuButtonInfo = uni.getMenuButtonBoundingClientRect()
+			console.log(miniMenuButtonInfo)
+			const navHeight = (miniMenuButtonInfo.bottom - mInfo.statusBarHeight) + (miniMenuButtonInfo.top - mInfo.statusBarHeight)
+			const navWidth = miniMenuButtonInfo.left
+			this.navBarHeight = navHeight
+			this.navBarWidth = navWidth
+			console.log(navHeight, navWidth)
+			// #endif
 		}
 	}
 </script>
@@ -27,14 +54,16 @@
 			left: 0;
 			top: 0;
 			z-index: 999;
-			display: flex;
-			justify-content: center;
-			align-items: center;
 			width: 100%;
-			height: 45px;
-			padding: 0 15px;
-			box-sizing: border-box;
 			background-color: $gk-base-color;
+			.navbar-content {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				height: 45px;
+				padding: 0 15px;
+				box-sizing: border-box;
+			}
 			.navbar-search {
 				display: flex;
 				align-items: center;
