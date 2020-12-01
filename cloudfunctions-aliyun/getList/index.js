e82b36d4ad60ad290c621eff83d1cc6622e85ed9
donglongfei <donglongfei2011@gmail.com>
2020-12-01 10:11:50 +0800

    切换选项卡懒加载数据

--- cloudfunctions-aliyun/getList/index.js
+++ cloudfunctions-aliyun/getList/index.js
@@ -6,13 +6,25 @@ exports.main = async (event, context) => {
 	//event为客户端上传的参数
 	// console.log('event : ', event)
 	
-	// 获取数据表 article 的数据，await要放在 async 中执行
+	// 接受分类，通过分类去筛选数据
+	const { name } = event
 	let listInfo = await db.collection('article')
-	.field({
-		// true 表示只返回这个字段，false 表示不返回，过滤掉
+	.aggregate()
+	.match({
+		classify: name
+	})
+	.project({
 		content: false
 	})
-	.get()
+	.end()
+	
+	// 获取数据表 article 的数据，await要放在 async 中执行
+	// let listInfo = await db.collection('article')
+	// .field({
+	// 	// true 表示只返回这个字段，false 表示不返回，过滤掉
+	// 	content: false
+	// })
+	// .get()
 	
 	//返回数据给客户端
 	return {
--- components/list-swiper/list-swiper.vue
+++ components/list-swiper/list-swiper.vue
@@ -3,7 +3,8 @@
 		<swiper class="home-swiper" :current="activeIndex" @change="handleChange">
 			<swiper-item class="swiper-item" v-for="(item, index) in tabNum" :key="index">
 				<!-- <text>{{ index }}</text> -->
-				<list-swiper-item :list="list"></list-swiper-item>
+				<!-- <list-swiper-item :list="list"></list-swiper-item> -->
+				<list-swiper-item :list="listCacheData[index]"></list-swiper-item>
 			</swiper-item>
 			<!-- <swiper-item class="swiper-item">
 				<list-swiper-item></list-swiper-item>
@@ -36,23 +37,47 @@
 		},
 		data() {
 			return {
-				list: []
+				list: [],
+				// listCacheData: []
+				listCacheData: {},
+				// listCacheData {
+				// 	0: {},
+				// 	1: {}
+				// }
 			};
 		},
+		watch: {
+			tabNum(newVal) {
+				if (newVal.length === 0) return
+				this.getListInfo(this.activeIndex)
+			}
+		},
 		created() {
-			this.getListInfo()
+			// this.getListInfo(0)
 		},
 		methods: {
 			handleChange(e) {
 				// console.log(e);
 				const { current } = e.detail
+				// console.log(this.tabNum[current].name)
+				// const name = this.tabNum[current].name
+				// this.getListInfo(name)
+				
+				this.getListInfo(current)
 				this.$emit('changeSwiper', current)
 			},
-			getListInfo() {
-				this.$api.getList().then((res) => {
+			getListInfo(current) {
+				this.$api.getList({
+					name: this.tabNum[current].name
+				})
+				.then((res) => {
 					const { data } = res
-					this.list = data
-					console.log(res)
+					// this.list = data
+					console.log(this.listCacheData)
+					// this.listCacheData[current] = data
+					// this.listCacheData.push(data)
+					this.$set(this.listCacheData, current, data)
+					console.log('请求数据：', data)
 				})
 			}
 		}
